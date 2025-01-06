4f250ba82b61e1443b876aa2bb456e62ca02f0f0
Shiv Chauhan <shivchauhan795@gmail.com>
2025-01-06 13:42:34 +0530

    bug fixing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,11 @@ import TypingEffect from './TypingEffect'
 const Home = () => {
   return (
 
-    <div className='flex flex-row flex-wrap justify-evenly items-center py-28 gap-10' id='home'>
+    <div className='flex flex-row flex-wrap justify-evenly items-center py-28 gap-12' id='home'>
 
-      <div className='flex flex-col items-start gap- p-2'>
+      <div className='flex flex-col items-start gap-2 pl-2'>
         <div className='flex items-center'>
-          <span className='shiv uppercase text-5xl p-3 gap-2 flex flex-col w-[450px] h-32'>
+          <span className='shiv uppercase text-5xl p-3 gap-2 flex flex-col w-[410px] h-48'>
             hi, i am
             <TypingEffect />
           </span>
